dbd5fae7015d89965c765efa3eeae3cc9490c069
dariozaz <74221880+dariozaz@users.noreply.github.com>
2021-04-13 22:26:10 -0500

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,9 @@
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDxLYShwjUFlJQraIl9aqSWQUUahuZ93NY",
+    authDomain: "pwadario.firebaseapp.com",
+    projectId: "pwadario",
+    storageBucket: "pwadario.appspot.com",
+    messagingSenderId: "480292562272",
+    appId: "1:480292562272:web:771c41b90700996d4b2a05"
+});
